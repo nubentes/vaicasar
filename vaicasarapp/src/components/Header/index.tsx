2bad2dc510ed9629818e2c4e2fb174bb3a5bc503
gportodev <gportodev@gmail.com>
2022-10-13 19:52:39 -0300

    Remove componente

--- vaicasarapp/src/components/Header/index.tsx
+++ vaicasarapp/src/components/Header/index.tsx
@@ -14,29 +14,27 @@ import {
 
 export default function Header() {
   return (
-    <Container>
-      <Img
-        source={{
-          uri: 'https://images.unsplash.com/photo-1501901609772-df0848060b33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0fHx8ZW58MHx8fHw%3D&w=1000&q=80',
-        }}
-        imageStyle={{ opacity: 0.5 }}
-      >
-        <Logo>
-          <Icon width={51.91} height={55.13} />
+    <Img
+      source={{
+        uri: 'https://images.unsplash.com/photo-1501901609772-df0848060b33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0fHx8ZW58MHx8fHw%3D&w=1000&q=80',
+      }}
+      imageStyle={{ opacity: 0.5 }}
+    >
+      <Logo>
+        <Icon width={51.91} height={55.13} />
 
-          <Title>Vai Casar?!</Title>
-        </Logo>
+        <Title>Vai Casar?!</Title>
+      </Logo>
 
-        <Info>
-          <Name>Ramon</Name>
+      <Info>
+        <Name>Ramon</Name>
 
-          <Date>15 de Maio de 2023</Date>
-        </Info>
+        <Date>15 de Maio de 2023</Date>
+      </Info>
 
-        <Banner>
-          <BannerInfo>223 dias</BannerInfo>
-        </Banner>
-      </Img>
-    </Container>
+      <Banner>
+        <BannerInfo>223 dias</BannerInfo>
+      </Banner>
+    </Img>
   );
 }
